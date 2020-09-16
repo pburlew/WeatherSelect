0ff7969475b7785bbef65685b81289e6a3b356e8
Paige Burlew <paigeburlew@gmail.com>
2020-09-16 18:09:31 -0500

    final touches

--- script.js
+++ script.js
@@ -13,41 +13,37 @@ $("#search-button").on("click", function (){
 
 })
 
-//add to history- NOT WORKING
-$(".document").on("click", function(){
-    // var pastSearch = $(this).text();
-    var pastSearch = $("<li>").addClass("li-text").text(searchValue);
-    $("#history").append(pastSearch);
-    // pastSearch = $("<li>").addClass("li");
-    console.log("history is working");
-    
-})
+function loadPrevSearched () {
+    searchedArray = [];
+    searchedArray = JSON.parse(window.localStorage.getItem("history"));
+    //if no previous searched cities, then stop/ do nothing
+    if (searchedArray === null) {
+        return
+    } else {
+        for (var i = 0; i < searchedArray.length; i++) {
+            //create a button for each value and append to the page
+            var prevSearchedBtn = $("<button>").text(searchedArray[i]).attr({
+                class: "psearch",
+            })
+            previousSearchesEl.append(prevSearchedBtn);
+        }
+    }
+
+}
+
 
 
 //functions
 
-//this function uses moment.js to add current day and time in the header- WORKING
+//this function uses moment.js to add current day and time in the header- 
 function showCurrentDay(){
 
     $("#timedate-reader").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
 }
 showCurrentDay();
 
-//this function adds a row to the history div to show previouly searched cities- NOT WORKING
-function makeRow(text){
 
-        $("#history").removeClass("hide");
-        //showing the last 5 cities searched
-        for (var i = 0; i < 5; i++) {
-            
-            //adding to list
-            $("<ul>").append("list-group history");
-            
-        }
-
-}
-
-//searches current weather- WORKING except icons 
+//searches current weather
 function searchWeather(searchValue) {
     $.ajax({
         //connecting to API for current weather
@@ -55,6 +51,7 @@ function searchWeather(searchValue) {
         url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=4c023acf398932e1b43cd03002ad8542",
         dataType: "json",
         success: function(data){
+            console.log(searchValue)
             //if the city we just searched is not already in history, push to history
             if (history.indexOf(searchValue)=== -1){
                 history.push(searchValue);
@@ -71,15 +68,16 @@ function searchWeather(searchValue) {
             var card = $("<div>").addClass("card");
             var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + "MPH");
             var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
-            // var kToF = ((data.main.temp − 273.15) × 9/5 + 32)
-            var temp = $("<p>").addClass("card-text").text("Temperature: " + data.main.temp + " F");
+            
+            var temp = $("<p>").addClass("card-text").text("Temperature: " + ((data.main.temp- 273.15) * 1.80 + 32).toFixed(1) + " F");
             var cardBody = $("<div>").addClass("card-body");
-            var img = $("#wicon").attr("src", "http://openweathermap.org/img/w/" + data.icon + ".png");
+            var img = $("#wicon").attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
           
+           
 
             //adding to page (top container for today)
             title.append(img);
-            cardBody.append(title, temp, humid, wind, img);
+            cardBody.append(title, temp, humid, wind);
             card.append(cardBody);
             $("#today").append(card);
 
@@ -89,7 +87,7 @@ function searchWeather(searchValue) {
     })
 }
 
-// searches 5day forecast- WORKING except icons
+// searches 5day forecast
 function getForecast(searchValue){
     $.ajax({
         //connect to forecast API
@@ -97,6 +95,7 @@ function getForecast(searchValue){
         url: "http://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=4c023acf398932e1b43cd03002ad8542",
         dataType: "json",
         success: function(data){
+            console.log(data)
         //make sure old content is emptied 
          $("#forecast").empty();
 
@@ -113,9 +112,9 @@ function getForecast(searchValue){
             var titleForecast = $("<h5>").addClass("card-title").text(data.name);
             // var cardForecast = $("<div>").addClass("card");
             var humidForecast = $("<p>").addClass("card-text").text("Humidity: " + data.list[i].main.humidity + "%");
-            var tempForecast = $("<p>").addClass("card-text").text("Temperature: " + data.list[i].main.temp_max + " F");
+            var tempForecast = $("<p>").addClass("card-text").text("Temperature: " + ((data.list[i].main.temp_max - 273.15) * 1.80 + 32).toFixed(1)  + " F");
             var forecastBody = $("<div>").addClass("forecast-body p-2");
-            var imgForecast = $("#wicon").attr("src", "http://openweathermap.org/img/w/" + data.list[i].weather.icon + ".png");
+            var imgForecast = $("#wicon").attr("src", "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png");
             
             addToCol.append(cardForecast.append(forecastBody.append(titleForecast, imgForecast, tempForecast, humidForecast)));
 
@@ -133,9 +132,12 @@ function getForecast(searchValue){
 
 
 function gtUVindex(lat, lon){
+    
+     
+
 
 }
-// get current history - NOT WORKING
+
 var history = JSON.parse(window.localStorage.getItem("history")) || [];
 
 if (history.length >0) {
@@ -144,4 +146,16 @@ if (history.length >0) {
 for (var i = 0; i < history.length; i++){
     makeRow(history[i]);
 }
- })
+
+function makeRow(){
+
+    $("#history").removeClass("hide");
+    //showing the last 5 cities searched
+    for (var i = 0; i < 5; i++) {
+        
+        //adding to list
+        $("<ul>").append("list-group history");
+        
+    }
+}
+})
